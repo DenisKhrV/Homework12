7da8a948e486915aea5a60e61afb8882af796e3a
DenisKhrV <DenisKhr@yandex.ru>
2023-07-12 09:51:49 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println("Hello world!");
+    }
+
+    public static void task1() {
+
+    }
+}
