4efde5777021845aeb8f917ab8d817026da3c101
Денис <DenisKhr@yandex.ru>
2023-07-12 19:48:51 +1000

    Внёс доработки.

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,6 @@
 public class Book {
-    private String bookTitle;
-    Author authorName;
+    private final String bookTitle;
+    private final Author authorName;
     private int publicationYear;
 
     public Book(String bookTitle, Author authorName, int publicationYear) {
@@ -14,6 +14,9 @@ public class Book {
     }
     public int getPublicationYear() {
         return this.publicationYear;
+    }
+    public Author getAuthor(){
+        return this.authorName;
     }
 
     public void setPublicationYear(int publicationYear) {
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         Author levTolstoy = new Author("Lev", "Tolstoy");
         Book warAndPeace = new Book("War and Peace", levTolstoy, 1867);
         System.out.println("warAndPeace.getBookTitle() = " + warAndPeace.getBookTitle());
-        System.out.println("levTolstoy.getFirstName() = " + levTolstoy.getFirstName());
-        System.out.println("levTolstoy.getLastName() = " + levTolstoy.getLastName());
+        System.out.println("warAndPeace.getAuthor() = " + warAndPeace.getAuthor().getFirstName());
+        System.out.println("warAndPeace.getAuthor().getLastName() = " + warAndPeace.getAuthor().getLastName());
         System.out.println("warAndPeace.getPublicationYear() = " + warAndPeace.getPublicationYear());
         warAndPeace.setPublicationYear(2002);
         System.out.println("warAndPeace.getPublicationYear() = " + warAndPeace.getPublicationYear());
@@ -14,8 +14,8 @@ public class Main {
         Author nikolayNosov = new Author("Nikolay", "Nosov");
         Book neznaykaNaLune = new Book("Dunno on the Moon", nikolayNosov, 1965);
         System.out.println("neznaykaNaLune.getBookTitle() = " + neznaykaNaLune.getBookTitle());
-        System.out.println("nikolayNosov.getFirstName() = " + nikolayNosov.getFirstName());
-        System.out.println("nikolayNosov.getLastName() = " + nikolayNosov.getLastName());
+        System.out.println("neznaykaNaLune.getAuthor().getFirstName() = " + neznaykaNaLune.getAuthor().getFirstName());
+        System.out.println("neznaykaNaLune.getAuthor().getLastName() = " + neznaykaNaLune.getAuthor().getLastName());
         System.out.println("neznaykaNaLune.getPublicationYear() = " + neznaykaNaLune.getPublicationYear());
 
     }
